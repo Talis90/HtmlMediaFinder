89cfd3bd5e1d3dc2af659cbe5e5ea051c813a598
antje <antje@debian>
2015-01-11 18:30:26 +0100

    directory seperator

--- library/HtmlMediaFinder/HtmlMediaFinder.php
+++ library/HtmlMediaFinder/HtmlMediaFinder.php
@@ -18,11 +18,11 @@ class HtmlMediaFinder
 		$toplevelDomain = parse_url($videoUrl);
 		$toplevelDomain = $toplevelDomain['host'];
 		
-		if (!is_dir(__DIR__ . '/ProviderHandler/' . $toplevelDomain)) {
+		if (!is_dir(__DIR__ . DIRECTORY_SEPARATOR . 'ProviderHandler' . DIRECTORY_SEPARATOR . $toplevelDomain)) {
 			throw new \Exception('There is no special implementation for the provider ' . $toplevelDomain . '!');
 		}
 		
-		require_once __DIR__ . '/ProviderHandler/' . $toplevelDomain . '/Provider.php';
+		require_once __DIR__ . DIRECTORY_SEPARATOR . 'ProviderHandler' . DIRECTORY_SEPARATOR . $toplevelDomain . DIRECTORY_SEPARATOR . 'Provider.php';
 		$provider = new \Provider($videoUrl);
 		return $provider->getDownloadUrl();
 	}
